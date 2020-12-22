4c7c79e380f6d7224cd5bc79eda05fe967f439b6
iliaxyn <72629016+iliaxyn@users.noreply.github.com>
2020-12-22 18:44:17 +0200

    Add files via upload

---
+++ node2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import rospy
+ 
+from std_msgs.msg import Int32
+from random import randint
+ 
+#a function to generate the random number
+def generate_random_number():
+    rnd2= randint(0,1000)
+    return rnd
+ 
+if __name__=='__main__':
+    rospy.init_node('random_number2')
+    pub=rospy.Publisher('rand_no2', Int32, queue_size=10)
+    rate= rospy.Rate(5)
+ 
+    while not rospy.is_shutdown():
+        rnd_gen=generate_random_number()
+       
+ 
+rate.sleep()
---
+++ node3.py
@@ -0,0 +1,54 @@
+NAME = 'select from 2 random numbs and print either the bigger or the prime'
+
+import rospy
+from std_msgs.msg import *
+
+
+def node3(req):
+
+if rnd1<rnd2
+ 
+if rnd1 > 1:
+   
+   for i in range(2,rnd1):
+       if (rnd1 % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num1,"is a prime number")
+       print(num1)
+
+else:
+   print(rnd2,"is not a prime number")
+   print(num2)
+}
+
+def node3():
+    rospy.init_node(NAME)
+
+
+
+
+  	rospy.init_node('node_3')
+ 	rospy.Subscriber("chatter1", Int32, callback)
+  
+	rospy.init_node('node_3')
+  	rospy.Subscriber("chatter2", Int32, callback)
+
+
+  
+
+    # spin() keeps Python from exiting until node is shutdown
+    rospy.spin()
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    node3()
